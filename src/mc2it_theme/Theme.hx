43a17343118280ff13d31da50df3009d5d83418d
Cédric Belin <cedric@belin.io>
2021-03-07 20:44:56 +0100

    Added the `Theme` class

---
+++ src/mc2it_theme/Theme.hx
@@ -0,0 +1,14 @@
+package mc2it_theme;
+
+/** Bootstrap theme used by MC2IT applications. **/
+class Theme {
+
+	/** The version number of the [Bootstrap](https://getbootstrap.com) library. **/
+	public static final bootstrap = "5.0.0-beta2";
+
+	/** The version number of the [Font Awesome](https://fontawesome.com) library. **/
+	public static final fontAwesome = "5.15.2";
+
+	/** The version number of this theme. **/
+	public static final version = "1.0.0";
+}

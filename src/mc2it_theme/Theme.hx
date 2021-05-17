b8f9aed121d89bd3baf66b77a81499dd429a1fa6
Cédric Belin <cedric@belin.io>
2021-05-17 11:47:12 +0200

    Met à jour les dépendances

--- src/mc2it_theme/Theme.hx
+++ src/mc2it_theme/Theme.hx
@@ -4,10 +4,10 @@ package mc2it_theme;
 class Theme {
 
 	/** The version number of the [Bootstrap](https://getbootstrap.com) library. **/
-	public static final bootstrap = "5.0.0-beta3";
+	public static final bootstrap = "5.0.1";
 
 	/** The version number of the [Bootstrap Icons](https://icons.getbootstrap.com) library. **/
-	public static final fontAwesome = "1.4.1";
+	public static final fontAwesome = "1.5.0";
 
 	/** The version number of this theme. **/
 	public static final version = "1.0.0";

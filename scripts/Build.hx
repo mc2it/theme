3f35f3e5f0b0d4899fcdbcfa618fce800066f281
Cédric Belin <cedric@belin.io>
2021-05-22 00:28:45 +0200

    Moved the assets to the "www" folder

--- scripts/Build.hx
+++ scripts/Build.hx
@@ -2,5 +2,5 @@ import Sys.*;
 
 /** Runs the script. **/
 function main() {
-	command("npx sass --load-path=node_modules --quiet --style=compressed src/mc2it_theme:css");
+	command("npx sass --load-path=node_modules --quiet --style=compressed src/mc2it_theme:www/css");
 }

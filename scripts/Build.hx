c64716181fa5b0721087f52718674bc0afb1c6df
Cédric Belin <cedric@belin.io>
2021-06-15 17:12:02 +0200

    Updated the "Build" task

--- scripts/Build.hx
+++ scripts/Build.hx
@@ -1,7 +1,20 @@
+//! --library bootstrap_bundle
 import Sys.*;
+import haxe.Exception;
+import sys.io.File.*;
+import sys.io.Process;
 
 /** Runs the script. **/
 function main() {
 	final bsDir = captureCommand("lix run bootstrap_bundle libpath");
-	command("npx sass --load-path=$bsDir --quiet-deps --style=compressed src/mc2it_theme:www/css");
+	command('npx sass --load-path=$bsDir --quiet-deps --no-source-map --style=compressed src/mc2it_theme:www/css');
+	copy('$bsDir/fonts/bootstrap-icons.woff2', "www/fonts/bootstrap_icons.woff2");
+}
+
+/** Captures the output of the specified `command`. **/
+private function captureCommand(command: String, ?arguments: Array<String>) {
+	final process = new Process(command, arguments);
+	final output = process.exitCode() == 0 ? process.stdout.readLine() : throw new Exception(process.stderr.readLine());
+	process.close();
+	return output;
 }

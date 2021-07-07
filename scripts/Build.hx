0896c373d43ede5d40dbd046e70fe84e369b46c9
Cédric Belin <cedric@belin.io>
2021-07-07 17:08:29 +0200

    Updated the stylesheet

--- scripts/Build.hx
+++ scripts/Build.hx
@@ -6,6 +6,5 @@ import sys.io.File.*;
 function main() {
 	final bsDir = captureCommand("lix run bootstrap_bundle libpath");
 	copy('$bsDir/fonts/bootstrap-icons.woff2', "www/fonts/bootstrap_icons.woff2");
-	for (style => file in ["compressed" => "main.min.css", "expanded" => "main.css"])
-		command('npx sass --load-path=$bsDir --no-source-map --style=$style src/mc2it_theme/ui/main.scss:www/css/$file');
+	command('npx sass --load-path=$bsDir --no-source-map --style=compressed src/mc2it_theme/ui:www/css');
 }

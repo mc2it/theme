7a0b1ad77d32b1b8a0577c00dc4965b3ec89f526
Cédric Belin <cedric@belin.io>
2021-07-08 11:27:55 +0200

    Added a `copy` CLI command

---
+++ src/mc2it_theme/cli/CopyCommand.hx
@@ -0,0 +1,39 @@
+package mc2it_theme.cli;
+
+import sys.FileSystem;
+import sys.io.File;
+
+using Lambda;
+using haxe.io.Path;
+
+/** Copy the library assets to a given directory. **/
+class CopyCommand {
+
+	/** Output usage information. **/
+	public var help = false;
+
+	/** Creates a new `copy` command. **/
+	public function new() {}
+
+	/** <directory> : The path to the output directory. **/
+	@:defaultCommand
+	public function run(rest: Rest<String>): Promise<Noise> {
+		if (help) return { Sys.println(Cli.getDoc(this)); Noise; };
+
+		final haxelibRun = Sys.getEnv("HAXELIB_RUN") == "1";
+		final requiredArgs = 1;
+		if (rest.length < requiredArgs || (haxelibRun && rest.length < requiredArgs + 1))
+			return new Error(BadRequest, "You must provide the path of the output directory.");
+
+		final folders = ["css", "fonts", "img"];
+		final output = rest[0].isAbsolute() ? rest[0] : Path.join([haxelibRun ? rest[rest.length - 1] : Sys.getCwd(), rest[0]]);
+		folders.iter(folder -> FileSystem.createDirectory(Path.join([output, folder])));
+
+		final input = Path.join([Sys.getCwd(), "www"]);
+		final files = ["favicon.ico", "favicon.svg"]
+			.concat(folders.map(folder -> FileSystem.readDirectory(Path.join([input, folder])).map(file -> '$folder/$file')).flatten());
+
+		files.iter(file -> File.copy(Path.join([input, file]), Path.join([output, file])));
+		return Noise;
+	}
+}

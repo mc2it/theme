80bbbb642048afd9bad27fbfb1e5f93651d5ad6e
Cédric Belin <cedric@belin.io>
2021-07-09 00:06:32 +0200

    Fixed the build system

--- scripts/Build.hx
+++ scripts/Build.hx
@@ -4,7 +4,10 @@ import sys.io.File.*;
 
 /** Runs the script. **/
 function main() {
-	final bsDir = captureCommand("lix run bootstrap_bundle libpath");
-	copy('$bsDir/fonts/bootstrap-icons.woff2', "www/fonts/bootstrap_icons.woff2");
-	command('npx sass --load-path=$bsDir --no-source-map src/mc2it_theme/ui:www/css');
+	final debug = args().contains("--debug") ? "--debug" : "";
+	command('haxe $debug run.hxml');
+
+	final bootstrapDir = captureCommand("lix run bootstrap_bundle libpath");
+	copy('$bootstrapDir/fonts/bootstrap-icons.woff2', "www/fonts/bootstrap_icons.woff2");
+	command('npx sass --load-path=$bootstrapDir --no-source-map src/mc2it_theme/ui:www/css');
 }

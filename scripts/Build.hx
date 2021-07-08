fda41532d91dd08b3e19bfcceecb54de873e8344
Cédric Belin <cedric@belin.io>
2021-07-08 12:05:48 +0200

    Using "clean-css" to generate the minified stylesheet

--- scripts/Build.hx
+++ scripts/Build.hx
@@ -6,5 +6,6 @@ import sys.io.File.*;
 function main() {
 	final bsDir = captureCommand("lix run bootstrap_bundle libpath");
 	copy('$bsDir/fonts/bootstrap-icons.woff2', "www/fonts/bootstrap_icons.woff2");
-	command('npx sass --load-path=$bsDir --no-source-map --style=compressed src/mc2it_theme/ui:www/css');
+	command('npx sass --load-path=$bsDir --no-source-map src/mc2it_theme/ui:www/css');
+	command("npx cleancss -O2 all:on --output=www/css/mc2it.min.css www/css/mc2it.css");
 }

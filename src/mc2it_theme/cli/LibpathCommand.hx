05b5781ffb74819933b56bf81b98cc84d0aa6b28
Cédric Belin <cedric@belin.io>
2021-07-08 18:29:20 +0200

    Updated the `libpath` CLI command

--- src/mc2it_theme/cli/LibpathCommand.hx
+++ src/mc2it_theme/cli/LibpathCommand.hx
@@ -9,13 +9,16 @@ class LibpathCommand {
 	/** Output usage information. **/
 	public var help = false;
 
+	/** Print the specific path to the Sass files. **/
+	public var scss = false;
+
 	/** Creates a new `libpath` command. **/
 	public function new() {}
 
 	// Runs this command.
 	@:defaultCommand
 	public function run() {
-		Sys.println(help ? Cli.getDoc(this) : Path.join([Sys.getCwd(), "www"]).replace("/", Sys.systemName() == "Windows" ? "\\" : "/"));
+		Sys.println(help ? Cli.getDoc(this) : Path.join([Sys.getCwd(), scss ? "src/ui" : "www"]).replace("/", Sys.systemName() == "Windows" ? "\\" : "/"));
 		return Promise.NOISE;
 	}
 }

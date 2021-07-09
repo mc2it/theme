e82ce13ccbdbc8a964c93afcb790f7e2bbb51d39
Cédric Belin <cedric@belin.io>
2021-07-09 18:03:36 +0200

    Updated the dependencies

--- scripts/Lint.hx
+++ scripts/Lint.hx
@@ -1,4 +1,7 @@
 import Sys.*;
 
 /** Runs the script. **/
-function main() command("lix run checkstyle --config etc/checkstyle.json --source scripts --source src");
+function main() {
+	command("lix run checkstyle --config etc/checkstyle.json --source scripts --source src");
+	command("npx lessc --lint src/mc2it_theme/ui/mc2it.less");
+}

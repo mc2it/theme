6ab944b1f2b8e0590c3bc948a13e684c38f2971a
Cédric Belin <cedric@belin.io>
2021-10-06 17:40:57 +0200

    Updated the "Dist" task

--- scripts/Dist.hx
+++ scripts/Dist.hx
@@ -3,5 +3,5 @@ import Sys.*;
 /** Runs the script. **/
 function main() {
 	for (script in ["Clean", "Version", "Build"]) command('lix $script');
-	command("npx cleancss -O2 --output=www/css/mc2it.min.css www/css/mc2it.css");
+	command("npx cleancss -O2 --output=www/css/mc2it.css www/css/mc2it.css");
 }

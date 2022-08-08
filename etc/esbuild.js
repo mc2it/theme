b6547b5617af2dbde169b1bd7145e2d9273f1ba8
Cédric Belin <cedric@belin.io>
2022-08-08 12:46:36 +0200

    Updated the dependencies

--- etc/esbuild.js
+++ etc/esbuild.js
@@ -1,14 +1,11 @@
 import {env} from "node:process";
 
-// Returns a value indicating whether the application runs in production mode.
-const isProduction = () => env.NODE_ENV == "production";
-
 /**
  * Returns the CSS settings.
  * @returns {import("esbuild").BuildOptions} The CSS settings.
  */
 export function cssOptions() {
-	return Object.assign(sharedOptions(isProduction()), {
+	return Object.assign(sharedOptions(env.NODE_ENV == "production"), {
 		entryPoints: ["src/ui/index.css"],
 		external: ["*.woff2"],
 		outfile: "www/css/mc2it.css",

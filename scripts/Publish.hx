61ca74ed914a87789b8732160416d1e5a19107e6
Cédric Belin <cedric@belin.io>
2023-02-14 19:51:57 +0100

    Updated the "Publish" task

--- scripts/Publish.hx
+++ scripts/Publish.hx
@@ -3,9 +3,9 @@ import mc2it_theme.Version;
 
 /** Runs the script. **/
 function main() {
-	Sys.command("lix Dist");
+	Sys.command("lix", ["Dist"]);
 	Tools.compress(["CHANGELOG.md", "LICENSE.md", "README.md", "haxelib.json", "run.n", "src", "www"], "var/haxelib.zip");
-	Sys.command("haxelib submit var/haxelib.zip");
+	Sys.command("haxelib", ["submit", "var/haxelib.zip"]);
 
 	final registries = ["https://registry.npmjs.org", "https://git.mc2it.com/api/packages/mc2it/npm/"];
 	for (registry in registries) Sys.command("npm", ["publish", '--registry=$registry']);

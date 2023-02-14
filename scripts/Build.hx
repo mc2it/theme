c4490bcd64fff4e48923d9b26774fe4f53ed845e
Cédric Belin <cedric@belin.io>
2023-02-14 21:17:39 +0100

    Updated the "Build" task

--- scripts/Build.hx
+++ scripts/Build.hx
@@ -11,6 +11,7 @@ function main() {
 	Tools.replaceInFile("src/mc2it_theme/ui/index.css", ~/".*\/css\/bootstrap.css"/, '"${Path.join([bootstrap, "css/bootstrap.css"])}"');
 	buildStyleSheet(false);
 	buildStyleSheet(true);
+	Sys.command("git", ["checkout", "--", "src/mc2it_theme/ui/index.css"]);
 
 	final file = "bin/mc2it_theme.js";
 	Sys.command("git", ["update-index", "--chmod=+x", file]);

233075ce90864b1ef8299fa5736c0190d668ca50
Cédric Belin <cedric@belin.io>
2023-02-15 15:54:13 +0100

    Optimisation de code

--- scripts/Build.hx
+++ scripts/Build.hx
@@ -1,12 +1,11 @@
 import sys.io.File;
-using Lambda;
 using haxe.io.Path;
 
 /** Runs the script. **/
 function main() {
 	final bootstrap = Tools.captureCommand("lix run bootstrap_bundle libpath");
 	File.copy(Path.join([bootstrap, "fonts/bootstrap-icons.woff2"]), "www/fonts/bootstrap_icons.woff2");
-	["build", "run"].iter(file -> Sys.command("haxe", ['$file.hxml']));
+	for (file in ["build", "run"]) Sys.command("haxe", ['$file.hxml']);
 
 	Tools.replaceInFile("src/mc2it_theme/ui/index.css", ~/".*\/css\/bootstrap.css"/, '"${Path.join([bootstrap, "css/bootstrap.css"])}"');
 	buildStyleSheet();

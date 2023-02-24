41595414513e38e6dc6b8f4980200be75941f533
Cédric Belin <cedric@belin.io>
2023-02-25 00:26:55 +0100

    Code formatting

--- scripts/Lint.hx
+++ scripts/Lint.hx
@@ -1,10 +1,5 @@
 /** Performs the static analysis of source code. **/
 function main() {
+	Sys.command("lix run checkstyle --config etc/checkstyle.json --exitcode --source scripts --source src");
 	Sys.command("npx stylelint --config=etc/stylelint.json src/mc2it_theme/ui/**/*.css");
-	Sys.command("lix", ["run", "checkstyle",
-		"--config", "etc/checkstyle.json",
-		"--exitcode",
-		"--source", "scripts",
-		"--source", "src"
-	]);
 }

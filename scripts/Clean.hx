21796a33f885c5683a6629f29776513782d3f988
Cédric Belin <cedric@belin.io>
2023-03-23 23:04:14 +0100

    Code optimization

--- scripts/Clean.hx
+++ scripts/Clean.hx
@@ -3,10 +3,8 @@ using Lambda;
 
 /** Deletes all generated files. **/
 function main() {
-	for (file in ["bin/mc2it_theme.js", "lib/index.js"]) {
-		if (FileSystem.exists(file)) FileSystem.deleteFile(file);
-		if (FileSystem.exists('$file.map')) FileSystem.deleteFile('$file.map');
-	}
+	for (file in ["bin/mc2it_theme.js", "lib/index.js"])
+		[file, '$file.map'].filter(FileSystem.exists).iter(FileSystem.deleteFile);
 
 	Tools.cleanDirectory("var");
 	if (FileSystem.exists("www/css")) Tools.removeDirectory("www/css");

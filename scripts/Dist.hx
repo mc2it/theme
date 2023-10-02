6eac2f806ca3aa9ddaf3c91ab6a709de0b1cc6b0
Cédric Belin <cedric@belin.io>
2023-10-02 16:04:49 +0200

    Update the "Dist" task

--- scripts/Dist.hx
+++ scripts/Dist.hx
@@ -5,6 +5,7 @@ using Lambda;
 function main() {
 	for (script in ["Clean", "Build", "Version"]) Sys.command('lix $script');
 	["bin/mc2it_theme.js", "lib/index.cjs"].iter(file -> minifyFile(file, true));
+	Sys.command("git update-index --chmod=+x bin/mc2it_theme.js");
 	minifyFile("www/css/mc2it.css", "www/css/mc2it.min.css");
 }
 

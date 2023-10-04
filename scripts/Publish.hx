3cf783ddc8dc442369c4cba581a347bcec1eab98
Cédric Belin <cedric@belin.io>
2023-10-04 17:14:25 +0200

    Update the "Publish" task

--- scripts/Publish.hx
+++ scripts/Publish.hx
@@ -7,7 +7,6 @@ function main() {
 	Tools.compress(["CHANGELOG.md", "LICENSE.md", "README.md", "haxelib.json", "run.n", "src", "www"], "var/haxelib.zip");
 	Sys.command("haxelib submit var/haxelib.zip");
 
-	final registries = ["https://registry.npmjs.org", "https://git.mc2it.com/api/packages/mc2it/npm/"];
-	for (registry in registries) Sys.command('npm publish --registry=$registry');
+	Sys.command("npm publish");
 	for (action in ["tag", "push origin"]) Sys.command('git $action v${Platform.packageVersion}');
 }

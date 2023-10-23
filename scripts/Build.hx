ef1ae1066cac836bb016c1bf5262569f1b65707b
Cédric Belin <cedric@belin.io>
2023-10-23 20:05:34 +0200

    Replace Bootstrap Icons by Material Symbols

--- scripts/Build.hx
+++ scripts/Build.hx
@@ -1,6 +1,5 @@
 //! --class-path src --define hxnodejs --define no-deprecation-warnings --library hxnodejs --library tink_core
 import js.esbuild.Esbuild;
-import js.node.ChildProcess;
 import sys.io.File;
 using StringTools;
 using haxe.io.Path;
@@ -11,8 +10,8 @@ function main() {
 	final debug = Sys.args().contains("--debug");
 	for (file in ["build", "run"]) Sys.command('haxe ${debug ? "--debug" : ""} $file.hxml');
 
-	final bootstrap = ChildProcess.execSync("lix run bootstrap_bundle libpath", {encoding: "utf8"}).rtrim();
-	File.copy(Path.join([bootstrap, "fonts/bootstrap-icons.woff2"]), "www/fonts/bootstrap_icons.woff2");
+	final fontsource = "node_modules/@fontsource-variable/material-symbols-rounded/files";
+	File.copy(Path.join([fontsource, "material-symbols-rounded-latin-wght-normal.woff2"]), "www/fonts/material_symbols.woff2");
 	Esbuild.build(Tools.buildOptions()).toPromise().handle(outcome -> switch outcome {
 		case Failure(error): throw error;
 		case Success(_): if (!debug) Tools.replaceInFile("www/css/mc2it.css", ~/\s+\/\* [^*]+ \*\//g, "");

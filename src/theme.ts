b4e34e8525a5825609ddfa0602d375a50808bd91
Cédric Belin <cedric@belin.io>
2024-02-02 13:10:39 +0100

    Start the port of the stylesheet to Sass

---
+++ src/theme.ts
@@ -0,0 +1,20 @@
+/**
+ * Defines the options of the `copyAssets()` function.
+ */
+export interface ThemeCopyOptions {
+
+	/**
+	 * Value indicating whether to only copy the CSS files.
+	 */
+	css: boolean;
+
+	/**
+	 * Value indicating whether to only copy the font files.
+	 */
+	fonts: boolean;
+
+	/**
+	 * Value indicating whether to only copy the image files.
+	 */
+	img: boolean;
+}

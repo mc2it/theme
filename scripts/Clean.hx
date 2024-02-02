d539f439deb77bde5a6864991a7e3146e5c24061
Cédric Belin <cedric@belin.io>
2024-02-02 18:00:20 +0100

    Remove obsolete files

--- scripts/Clean.hx
+++
@@ -1,9 +0,0 @@
-import sys.FileSystem;
-using Lambda;
-
-/** Deletes all generated files. **/
-function main() {
-	for (file in ["bin/mc2it_theme.js", "lib/index.cjs"]) [file, '$file.map'].filter(FileSystem.exists).iter(FileSystem.deleteFile);
-	Tools.cleanDirectory("var");
-	if (FileSystem.exists("www/css")) Tools.removeDirectory("www/css");
-}

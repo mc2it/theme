2ad8fe0e6d2281bdab40b0237c2416e6ddf18b2b
Cédric Belin <cedric@belin.io>
2024-03-02 15:13:00 +0100

    Update the table styles

--- tool/build.js
+++
@@ -1,6 +0,0 @@
-import {cpSync} from "node:fs";
-import {join} from "node:path";
-
-// Builds the project.
-const fontsource = "node_modules/@fontsource-variable/material-symbols-rounded/files";
-cpSync(join(fontsource, "material-symbols-rounded-latin-fill-normal.woff2"), "www/fonts/material_symbols.woff2");

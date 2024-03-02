4745b7fea30a5b0fd6148587e2342793bbfc0567
Cédric Belin <cedric@belin.io>
2024-03-02 14:24:25 +0100

    Add the Sass build script

---
+++ etc/sass.js
@@ -0,0 +1,21 @@
+import {mkdir, writeFile} from "node:fs/promises";
+import {EOL} from "node:os";
+import {env} from "node:process";
+import {compileAsync, NodePackageImporter} from "sass-embedded";
+
+/**
+ * Compiles the style sheet.
+ * @returns {Promise<void>} Resolves when the style sheet has been compiled.
+ */
+export default async function compileSass() {
+	const production = env.NODE_ENV == "production";
+	const {css, sourceMap} = await compileAsync("src/ui/index.scss", {
+		importers: [new NodePackageImporter],
+		sourceMap: !production,
+		style: production ? "compressed" : "expanded"
+	});
+
+	await mkdir("www/css", {recursive: true});
+	if (sourceMap) await writeFile("www/css/main.css.map", JSON.stringify(sourceMap));
+	return writeFile("www/css/mc2it.css", sourceMap ? `${css}${EOL}/*# sourceMappingURL=mc2it.css.map */` : css);
+}

9d8dd9cc1b8e5d9614ddd00e10c2fa386e7691f2
Cédric Belin <cedric@belin.io>
2024-07-13 00:12:37 +0200

    Fix a regression

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,59 @@
-export * from "./theme.js";
+import {cp} from "node:fs/promises";
+import {join} from "node:path";
+
+/**
+ * Returns the path to the library assets.
+ * @param options The path options.
+ * @returns The path to the library assets.
+ */
+export function assetPath(options: Partial<PathOptions> = {}): string {
+	return join(import.meta.dirname, options.scss ? "../src/ui" : "../www");
+}
+
+/**
+ * Copies the library assets to a given directory.
+ * @param output The path to the output directory.
+ * @param options The copy options.
+ * @returns Resolves when the assets have been copied.
+ */
+export async function copyAssets(output: string, options: Partial<CopyOptions> = {}): Promise<void> {
+	const sources = ["css", "fonts", "img"];
+	let directories = sources.filter(source => (options as Record<string, boolean>)[source]);
+	if (!directories.length) directories = sources;
+
+	const input = join(import.meta.dirname, "../www");
+	for (const directory of directories)
+		await cp(join(input, directory), directories.length == 1 ? output : join(output, directory), {recursive: true});
+}
+
+/**
+ * Defines the options of the {@link copyAssets} function.
+ */
+export interface CopyOptions {
+
+	/**
+	 * Value indicating whether to copy only the CSS files.
+	 */
+	css: boolean;
+
+	/**
+	 * Value indicating whether to copy only the font files.
+	 */
+	fonts: boolean;
+
+	/**
+	 * Value indicating whether copy only the image files.
+	 */
+	img: boolean;
+}
+
+/**
+ * Defines the options of the {@link assetPath} function.
+ */
+export interface PathOptions {
+
+	/**
+	 * Value indicating whether to return the specific path for SCSS files.
+	 */
+	scss: boolean;
+}

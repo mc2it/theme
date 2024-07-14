d53fbe3a584bbf32773537a63bfe03dcb15ee086
Cédric Belin <cedric@belin.io>
2024-07-14 10:21:24 +0200

    Add a "watch" task

---
+++ etc/sass.js
@@ -0,0 +1,23 @@
+import {mkdir, writeFile} from "node:fs/promises";
+import {EOL} from "node:os";
+import {env} from "node:process";
+import {compileAsync, NodePackageImporter} from "sass-embedded";
+
+/**
+ * Compiles the style sheet.
+ * @returns {Promise<void>} Resolves when the style sheet has been compiled.
+ */
+export default async function compileSass() {
+	const production = env.NODE_ENV == "production";
+	const {css, sourceMap} = await compileAsync("src/ui/index.scss", {
+		importers: [new NodePackageImporter],
+		silenceDeprecations: ["mixed-decls"],
+		sourceMap: !production,
+		sourceMapIncludeSources: false,
+		style: production ? "compressed" : "expanded"
+	});
+
+	await mkdir("www/css", {recursive: true});
+	if (sourceMap) await writeFile("www/css/mc2it.css.map", JSON.stringify(sourceMap));
+	return writeFile("www/css/mc2it.css", sourceMap ? `${css}${EOL}/*# sourceMappingURL=mc2it.css.map */` : css);
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
-import {cp, mkdir, writeFile} from "node:fs/promises";
+import {cp} from "node:fs/promises";
 import {join} from "node:path";
+import {env} from "node:process";
 import {deleteAsync} from "del";
 import {execa} from "execa";
 import gulp from "gulp";
-import {compileAsync, NodePackageImporter} from "sass-embedded";
 import pkg from "./package.json" with {type: "json"};
+import compileSass from "./etc/sass.js";
 
 // Runs a command.
 const $ = execa({preferLocal: true, stdio: "inherit"});
@@ -13,18 +14,18 @@ const $ = execa({preferLocal: true, stdio: "inherit"});
 export async function build() {
 	const fontsource = "node_modules/@fontsource-variable/material-symbols-rounded/files";
 	await cp(join(fontsource, "material-symbols-rounded-latin-fill-normal.woff2"), "www/fonts/material_symbols.woff2");
-
-	const importers = [new NodePackageImporter];
-	const {css} = await compileAsync("src/ui/index.scss", {importers, silenceDeprecations: ["mixed-decls"], style: "compressed"});
-	await mkdir("www/css", {recursive: true});
-	await writeFile("www/css/mc2it.css", css);
-
-	return $`tsc --project src/tsconfig.json`;
+	return Promise.all([compileSass(), $`tsc --project src/tsconfig.json`]);
 }
 
 // Deletes all generated files.
 export function clean() {
 	return deleteAsync(["lib", "var/**/*", "www/css", "www/fonts/material_symbols.woff2"]);
+}
+
+// Packages the application.
+export function dist() {
+	env.NODE_ENV = "production";
+	return build();
 }
 
 // Performs the static analysis of source code.
@@ -41,8 +42,19 @@ export async function publish() {
 	for (const action of [["tag"], ["push", "origin"]]) await $`git ${action} v${pkg.version}`;
 }
 
+// Watches for file changes.
+export async function watch() {
+	await build();
+
+	const buildApp = () => $`tsc --sourceMap --project src/tsconfig.json`;
+	gulp.watch("src/**/*.ts", buildApp);
+
+	const buildStyleSheet = () => compileSass();
+	gulp.watch("src/ui/**/*.scss", buildStyleSheet);
+}
+
 // The default task.
 export default gulp.series(
 	clean,
-	build
+	dist
 );

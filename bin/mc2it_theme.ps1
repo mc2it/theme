7b778d858befd60c3d9b725fdcdd8309fe64f01b
Cédric Belin <cedric@belin.io>
2024-10-25 18:19:01 +0200

    Update the PowerShell script

--- bin/mc2it_theme.ps1
+++ bin/mc2it_theme.ps1
@@ -1,6 +1,3 @@
 #!/usr/bin/env pwsh
 Set-StrictMode -Version Latest
-
-$file = "$PSScriptRoot/mc2it_theme.js"
-if (Test-Path "$file.map") { node --enable-source-maps $file @args }
-else { node $file @args }
+neko "$PSScriptRoot/../run.n" @args

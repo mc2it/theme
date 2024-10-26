bbc058631c675651a498e1484566b6325f2dc6d0
Cédric Belin <cedric@belin.io>
2024-10-26 13:37:44 +0200

    Add the TS typings

---
+++ lib/index.d.ts
@@ -0,0 +1,45 @@
+/**
+ * Returns the path to the theme assets.
+ * @param options The path options.
+ * @returns The path to the theme assets.
+ */
+export function assetPath(options?: PathOptions): string;
+
+/**
+ * Copies the theme assets to a given `output` directory.
+ * @param output The path to the output directory.
+ * @param options The copy options.
+ */
+export function copyAssets(output: string, options?: CopyOptions): void;
+
+/**
+ * Defines the options of the {@link copyAssets} function.
+ */
+export type CopyOptions = Partial<{
+
+	/**
+	 * Value indicating whether to only copy the CSS files
+	 */
+	css: boolean;
+
+	/**
+	 * Value indicating whether to only copy the font files
+	 */
+	fonts: boolean;
+
+	/**
+	 * Value indicating whether to only copy the image files
+	 */
+	img: boolean;
+}>;
+
+/**
+ * Defines the options of the {@link assetPath} function.
+ */
+export type PathOptions = Partial<{
+
+	/**
+	 * Value indicating whether to return the specific of SCSS files.
+	 */
+	scss: boolean;
+}>;

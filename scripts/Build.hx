cbb39f4a2652016cc781b3fe995d932ac17be267
Cédric Belin <cedric@belin.io>
2024-10-27 13:23:13 +0100

    Update the "Build" script

--- scripts/Build.hx
+++ scripts/Build.hx
@@ -1,11 +1,10 @@
-//! --define hxnodejs --define no-deprecation-warnings --library bootstrap_bundle --library hxnodejs --library tink_core
+//! --define hxnodejs --define no-deprecation-warnings --library bootstrap_bundle --library hxnodejs
+import bootstrap.SassImporter;
 import haxe.Json;
-import js.sass.Importer.BootstrapImporter;
 import js.sass.Sass;
 import sys.FileSystem;
 import sys.io.File;
 using haxe.io.Path;
-using tink.CoreApi;
 
 /** Builds the project. **/
 function main() {
@@ -13,16 +12,14 @@ function main() {
 	File.copy(Path.join([fontsource, "material-symbols-rounded-latin-fill-normal.woff2"]), "www/fonts/material_symbols.woff2");
 
 	final debug = Sys.args().contains("--debug");
-	compileSass(debug).handle(outcome -> switch outcome {
-		case Failure(error): Sys.println(error);
-		case Success(_): for (file in ["build", "run"]) Sys.command('haxe ${debug ? "--debug" : ""} $file.hxml');
-	});
+	for (file in ["build", "run"]) Sys.command('haxe ${debug ? "--debug" : ""} $file.hxml');
+	compileSass(debug);
 }
 
 /** Compiles the Sass stylesheet. **/
-private function compileSass(debug: Bool): Promise<Noise> {
-	final promise = Sass.compileAsync("src/mc2it/theme/ui/index.scss", {
-		importers: [new BootstrapImporter()],
+private function compileSass(debug: Bool) {
+	final result = Sass.compile("src/mc2it/theme/ui/index.scss", {
+		importers: [{findFileUrl: new SassImporter().findFileUrl}],
 		silenceDeprecations: [ColorFunctions, GlobalBuiltin, Import, MixedDeclarations],
 		sourceMap: debug,
 		sourceMapIncludeSources: false,
@@ -30,9 +27,6 @@ private function compileSass(debug: Bool): Promise<Noise> {
 	});
 
 	FileSystem.createDirectory("www/css");
-	return promise.toPromise().next(result -> {
-		File.saveContent("www/css/mc2it.css", '${result.css}${result.sourceMap != null ? "\n/*# sourceMappingURL=mc2it.css.map */" : ""}');
-		if (result.sourceMap != null) File.saveContent("www/css/mc2it.css.map", Json.stringify(result.sourceMap));
-		Noise;
-	});
+	File.saveContent("www/css/mc2it.css", '${result.css}${result.sourceMap != null ? "\n/*# sourceMappingURL=mc2it.css.map */" : ""}');
+	if (result.sourceMap != null) File.saveContent("www/css/mc2it.css.map", Json.stringify(result.sourceMap));
 }

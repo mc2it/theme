3eaeca8c5fa4a2c4a0ebcff298e25ee5bbc22ab7
Cédric Belin <cedric@belin.io>
2024-10-27 15:24:28 +0100

    Replace "esbuild" by "terser" for minification

--- scripts/Dist.hx
+++ scripts/Dist.hx
@@ -6,12 +6,11 @@ using haxe.io.Path;
 
 /** Packages the project. **/
 function main() {
-	final cli = "bin/mc2it_theme.js";
 	for (script in ["Clean", "Build", "Version"]) Sys.command('lix $script');
 	copyDirectory("src/mc2it/theme/ui", "lib/scss");
 
-	final files = [cli, "lib/bundle.js"];
-	files.iter(file -> Sys.command('npx esbuild --allow-overwrite --legal-comments=none --log-level=warning --minify --outfile=$file --platform=node $file'));
+	final cli = "bin/mc2it_theme.js";
+	for (file in [cli, "lib/bundle.js"]) Sys.command('npx terser --comments=false --compress --mangle --output=$file $file');
 	Sys.command('git update-index --chmod=+x $cli');
 }
 

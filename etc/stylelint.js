4df6f2b7668065e7b270e774d4aa254e9a75defd
Cédric Belin <cedric@belin.io>
2024-11-03 23:45:57 +0100

    Update the settings of the static analyzer

---
+++ etc/stylelint.js
@@ -0,0 +1,10 @@
+export default {
+	extends: "stylelint-config-standard-scss",
+	rules: {
+		"color-function-notation": ["modern", {ignore: ["with-var-inside"]}],
+		"declaration-block-single-line-max-declarations": 2,
+		"declaration-empty-line-before": "never",
+		"media-feature-name-no-unknown": [true, {ignoreMediaFeatureNames: ["map"]}],
+		"scss/dollar-variable-empty-line-before": ["never", {ignore: ["after-dollar-variable"]}]
+	}
+};

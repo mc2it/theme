b1438b3b2211bfdf38fd8c4255c03dd79557fd7a
Cédric Belin <cedric@belin.io>
2025-01-28 02:11:18 +0100

    Move the launche script

--- bin/theme.ps1
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env pwsh
-Set-StrictMode -Version Latest
-
-$file = "$PSScriptRoot/theme.js"
-if (Test-Path "$PSScriptRoot/../lib/cli.js.map") { node --enable-source-maps $file @args }
-else { node $file @args }

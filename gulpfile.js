643d680d5b3a4945fe641b977201ffc385126817
Cédric Belin <cedric@belin.io>
2025-05-11 23:19:36 +0200

    Naming convention

--- gulpfile.js
+++ gulpfile.js
@@ -14,8 +14,8 @@ export async function assets() {
 /** Builds the project. */
 export async function build() {
 	await assets();
-	await run("npx", "tsc", ...typeScriptArguments());
-	await run("npx", "sass", ...sassArguments());
+	await run(`npx tsc ${typeScriptArguments()}`);
+	await run(`npx sass ${sassArguments()}`);
 }
 
 /** Deletes all generated files. */
@@ -33,23 +33,23 @@ export async function dist() {
 
 /** Performs the static analysis of source code. */
 export async function lint() {
-	await run("npx", "tsc", "--build", "tsconfig.json", "--noEmit");
-	await run("npx", "eslint", "--config=etc/ESLint.js", "gulpfile.js", "bin", "src");
+	await run("npx tsc --build tsconfig.json --noEmit");
+	await run("npx eslint --config=etc/ESLint.js gulpfile.js bin src");
 }
 
 /** Publishes the package. */
 export async function publish() {
-	await run("npx", "gulp");
-	for (const action of [["tag"], ["push", "origin"]]) await run("git", ...action, `v${pkg.version}`);
-	for (const registry of ["https://registry.npmjs.org", "https://npm.pkg.github.com"]) await run("npm", "publish", `--registry=${registry}`);
+	await run("npx gulp");
+	for (const action of ["tag", "push origin"]) await run(`git ${action} v${pkg.version}`);
+	for (const registry of ["https://registry.npmjs.org", "https://npm.pkg.github.com"]) await run(`npm publish --registry=${registry}`);
 }
 
 /** Watches for file changes. */
 export async function watch() {
 	env.NODE_ENV = "Development";
 	await assets();
-	void run("npx", "tsc", ...typeScriptArguments({watch: true}));
-	void run("npx", "sass", ...sassArguments({watch: true}));
+	void run(`npx tsc ${typeScriptArguments({watch: true})}`);
+	void run(`npx sass ${sassArguments({watch: true})}`);
 }
 
 /** The default task. */
@@ -58,37 +58,36 @@ export default gulp.series(clean, dist);
 /**
  * Spawns a new process using the specified command.
  * @param {string} command The command to run.
- * @param {...string} args The command arguments.
  * @returns {Promise<void>} Resolves when the command is terminated.
  */
-function run(command, ...args) {
+function run(command) {
 	return new Promise((resolve, reject) => {
-		const process = spawn(command, args, {shell: true, stdio: "inherit"});
-		process.on("close", code => code ? reject(Error([command, ...args].join(" "))) : resolve());
+		const process = spawn(command, {shell: true, stdio: "inherit"});
+		process.on("close", code => code ? reject(Error(command)) : resolve());
 	});
 }
 
 /**
  * Gets the Sass build arguments.
  * @param {{watch?: boolean}} options Value indicating whether to enable file watching.
- * @returns {string[]} The arguments to be passed to the Sass command line.
+ * @returns {string} The arguments to be passed to the Sass command line.
  */
 function sassArguments(options = {}) {
 	const args = ["--pkg-importer=node", "--quiet-deps", "--silence-deprecation=import"];
 	args.push(...env.NODE_ENV == "Production" ? ["--no-source-map", "--style=compressed"] : ["--source-map-urls=absolute"]);
 	if (options.watch) args.push("--watch");
 	args.push("src/UI/Main.scss:www/Styles/Mc2it.css");
-	return args;
+	return args.join(" ");
 }
 
 /**
  * Gets the TypeScript build arguments.
  * @param {{watch?: boolean}} options Value indicating whether to enable file watching.
- * @returns {string[]} The arguments to be passed to the TypeScript command line.
+ * @returns {string} The arguments to be passed to the TypeScript command line.
  */
 function typeScriptArguments(options = {}) {
 	const args = ["--build", "src/Cli/tsconfig.json"];
 	if (env.NODE_ENV != "Production") args.push("--sourceMap");
 	if (options.watch) args.push("--preserveWatchOutput", "--watch");
-	return args;
+	return args.join(" ");
 }

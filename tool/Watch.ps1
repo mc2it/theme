346322cf283c707dc648da885aa73316210f816c
Cédric Belin <cedx@outlook.com>
2025-10-08 22:34:29 +0200

    Code formatting

--- tool/Watch.ps1
+++ tool/Watch.ps1
@@ -1,8 +1,8 @@
 . $PSScriptRoot/Assets.ps1
 
 Write-Host "Watching for file changes..."
-$cliCommand = "npx tsc --build src/Cli/tsconfig.json --preserveWatchOutput --sourceMap --watch"
-$cliProcess = Start-Process pwsh "-Command $cliCommand" -NoNewWindow -PassThru
-$uiCommand = "npx esbuild src/UI/Main.css --bundle --external:*.woff2 --legal-comments=none --outfile=www/Styles/Mc2it.css --sourcemap --watch"
-$uiProcess = Start-Process pwsh "-Command $uiCommand" -NoNewWindow -PassThru
-Wait-Process $cliProcess.Id, $uiProcess.Id
+$esbuildCommand = "npx esbuild src/UI/Main.css --bundle --external:*.woff2 --legal-comments=none --outfile=www/Styles/Mc2it.css --sourcemap --watch"
+$esbuildProcess = Start-Process pwsh "-Command $esbuildCommand" -NoNewWindow -PassThru
+$tscCommand = "npx tsc --build src/Cli/tsconfig.json --preserveWatchOutput --sourceMap --watch"
+$tscProcess = Start-Process pwsh "-Command $tscCommand" -NoNewWindow -PassThru
+Wait-Process $esbuildProcess.Id, $tscProcess.Id

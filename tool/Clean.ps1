b0423f252d819ab7ef456012d693b1d1c8629d81
Cédric Belin <cedx@outlook.com>
2025-10-09 00:05:39 +0200

    Code optimization

--- tool/Clean.ps1
+++ tool/Clean.ps1
@@ -1,5 +1,4 @@
 Write-Host "Deleting all generated files..."
 if (Test-Path "lib") { Remove-Item "lib" -Force -Recurse }
-if (Test-Path "www/Fonts/MaterialSymbols.woff2") { Remove-Item "www/Fonts/MaterialSymbols.woff2" -Force }
 if (Test-Path "www/Styles") { Remove-Item "www/Styles" -Force -Recurse }
 Remove-Item "var/*" -Exclude ".gitkeep" -Force -Recurse

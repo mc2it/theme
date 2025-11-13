ab2800d9a4fe72e7e326ea43bf01fd192c514626
Cédric Belin <cedx@outlook.com>
2025-11-13 18:11:28 +0100

    Update the "Clean" script

--- tool/Clean.ps1
+++ tool/Clean.ps1
@@ -1,4 +1,3 @@
 "Deleting all generated files..."
-if (Test-Path "lib") { Remove-Item "lib" -Force -Recurse }
-if (Test-Path "www/Styles") { Remove-Item "www/Styles" -Force -Recurse }
+"lib", "www/Styles" | Remove-Item -ErrorAction Ignore -Force -Recurse
 Remove-Item "var/*" -Exclude ".gitkeep" -Force -Recurse

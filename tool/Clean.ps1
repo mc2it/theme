94e7de42a8a2ade8e63fc5c1fc54ef735cadac9b
Cédric Belin <cedx@outlook.com>
2026-01-21 02:02:27 +0100

    Code formatting

--- tool/Clean.ps1
+++ tool/Clean.ps1
@@ -1,3 +1,3 @@
 "Deleting all generated files..."
-"lib", "www/Styles" | Remove-Item -ErrorAction Ignore -Force -Recurse
-Remove-Item "var/*" -Exclude ".gitkeep" -Force -Recurse
+Remove-Item lib, www/Styles -ErrorAction Ignore -Force -Recurse
+Remove-Item var/* -Exclude .gitkeep -Force -Recurse

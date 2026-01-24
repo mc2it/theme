0a821f320a96b07451ba410f0360d7e9b8e7781b
Cédric Belin <cedx@outlook.com>
2026-01-24 16:34:05 +0100

    Update the "Watch" script

--- tool/Watch.ps1
+++ tool/Watch.ps1
@@ -2,7 +2,7 @@
 
 "Watching for file changes..."
 $esbuildCommand = "npx esbuild src/UI/Main.css --bundle --external:*.woff2 --legal-comments=none --outfile=www/Styles/Mc2it.css --sourcemap --watch"
-$esbuildProcess = Start-Process pwsh "-Command $esbuildCommand" -NoNewWindow -PassThru
+Start-Process pwsh "-Command $esbuildCommand" -NoNewWindow
 $tscCommand = "npx tsc --build src/Cli/tsconfig.json --preserveWatchOutput --sourceMap --watch"
-$tscProcess = Start-Process pwsh "-Command $tscCommand" -NoNewWindow -PassThru
-Wait-Process $esbuildProcess.Id, $tscProcess.Id
+Start-Process pwsh "-Command $tscCommand" -NoNewWindow
+Wait-Event ProcessTerminated

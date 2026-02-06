4fe802f8fe554f8c720a2d125b9bb28aa183ec88
Cédric Belin <cedx@outlook.com>
2026-02-07 00:29:46 +0100

    Remove the Visual Studio solution [skip ci]

--- tool/Clean.ps1
+++ tool/Clean.ps1
@@ -1,3 +1,3 @@
 "Deleting all generated files..."
-Remove-Item lib, obj, www/Styles -ErrorAction Ignore -Force -Recurse
+Remove-Item lib, www/Styles -ErrorAction Ignore -Force -Recurse
 Remove-Item var/* -Exclude .gitkeep -Force -Recurse
